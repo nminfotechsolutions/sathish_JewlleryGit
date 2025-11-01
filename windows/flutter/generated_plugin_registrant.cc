08f06563725d6ef9656dfc55d9ecb12cbda0ce46
nminfotechsolutions <nminfotechsolutions@gmail.com>
2025-11-01 13:26:20 +0530

    vignesh changes

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
+#include <mssql_connection/mssql_connection_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  MssqlConnectionPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MssqlConnectionPluginCApi"));
 }
